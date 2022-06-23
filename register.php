7bf9f96e98fd903749550f7071fad438443867dd
Anthony <anthony.mieville@jobtrek.ch>
2022-06-23 14:48:25 +0200

    Update README.md

---
+++ register.php
@@ -0,0 +1,18 @@
+
+<?php
+    require_once 'database_connecting.php';
+
+    if(isset($_POST['pseudo']) && isset($_POST['email']) && isset($_POST['password']) && isset($_POST['password_retype']))
+?>
+
+
+
+
+
+
+
+
+
+
+
+
